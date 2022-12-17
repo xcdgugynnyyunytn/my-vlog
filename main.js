6bc9701e8b586432381f2c7d3b96683624112537
xcdgugynnyyunytn <116884381+xcdgugynnyyunytn@users.noreply.github.com>
2022-12-17 17:49:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,69 +1,35 @@
-prediction_1=""
-prediction_2=""
-
-Webcam.set({
-    width:350,
-    height:300,
-    image_format:'png',
-    png_quality:90
-});
-
-camera=document.getElementById("camera");
-
-Webcam.attach('#camera');
-
-function take_snapshot(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML=' <img id="captured_image" src=" '+data_uri+' "/> ';
-
-    });
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=ml5.imageClassifier('MobileNet',  modelLoaded);
 }
-console.log('ml5 version:', ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/vD_A7GyAF/ model.json',modelLoaded);
+ function modelLoaded(){
+ console.log('Modal loaded!');
+ }
 
-function modelLoaded(){
-    comsole.log('ModelLoaded');
-}
-function speak(){
-    var synth=window.speechSynthesis;
-    speak_data_1="the first prediction is"+prediction_1;
-    speak_data_2=" and the second prediction is"+prediction_2;
-    var utterThis=new SpeechSynthesisUtterance (speak_data_1+speak_data_2);
-    synth.speak(utterThis);
-}
-function check(){
-    img=document.getElementById('captured_image');
-    classifier.classify(img,gotResult);
-}
-function gotResult(error,results){
-    if(error){
-        console.error(error);
+ function draw(){
+image(video,0,0,300,300);
+classifier.classify(video,gotResult);
+ }
+var previous_result='';
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    if((results[0].confidence > 0.5) && (previous_result != results[0].label))
+    {
+      console.log(results);
+      previous_result = results[0].label;
+      var synth = window.speechSynthesis;
+      speak_data = 'Object detected is - '+results[0].label;
+      var utterThis = new SpeechSynthesisUtterance(speak_data);
+      synth.speak(utterThis);
+      
+document.getElementById("result_object_name").innerHTML = results[0].label;
+document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
     }
-    else{
-        console.log(results);
-        document.getElementById("result_emotion_name").innerHTML=results[0].label;
-        document.getElementById("result_emotion_name2").innerHTML=results[1].label;
-        predction_1=results[0].label;
-        predction_2=results[1].label;
-        speak();
-        if(results[0].label=="THUMBS UP"){
-        document.getElementById("update_emoji").innerHTML="&#128522;";
-        }
-        if(results[0].label=="VICTORY"){
-            document.getElementById("update_emoji").innerHTML="&#532;";
-            }
-            if(results[0].label=="NICE"){
-                document.getElementById("update_emoji").innerHTML="&#128548;";
-                }
-
-                if(results[1].label=="THUMBS UP"){
-                    document.getElementById("update_emoji2").innerHTML="&#128522;";
-                    }
-                    if(results[1].label=="VICTORY"){
-                        document.getElementById("update_emoji2").innerHTML="&#532;";
-                        }
-                        if(results[1].label=="NICE"){
-                            document.getElementById("update_emoji2").innerHTML="&#128548;";
-                            }
-                }
-    }
+  }
+}
